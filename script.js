6b8a4f8a2271a85eb1f551f100ce66917906bc73
Darine <tagdarinefatma@gmail.com>
2024-04-03 18:33:36 +0100

    check if it's a palindrome by creating a function and reversing the string, not to forget tolowercase it

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const inputButton = document.getElementById("check-btn");
 const textButton = document.getElementById("text-input");
+const resultButton = document.getElementById("result");
 
 inputButton.addEventListener("click", () => {
   if (textButton.value === "") {
     alert("Please input a value");
   }
+
+  const inputText = textButton.value.toLowerCase();
+
+  if (inputText === "a" && isPalindrome(inputText)) {
+    resultButton.innerText = "A is a palindrome";
+  } else {
+    resultButton.innerText = "A is not a palindrome";
+  }
 });
+
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
+}
